505cb6037ffce8fe4823177e0dba046032aca43d
156237687 <jasonyao132@gmail.com>
2025-04-23 18:16:54 -0400

    Create memberbios.js for bio info

---
+++ memberbios.js
@@ -0,0 +1,100 @@
+const memberbios = [
+    {
+        name: "Krishiv Patel",
+        bio: "Krishiv is a young scholar from Florida that wants to make an impact in the world. As he continues to positively impact society in many ways he is also joining many initiatives to boost help in underprivileged areas.",
+        departments: "Chapter Founder, Website Developer, Website Designer, Outreach and Recruiting",
+        country: "USA",
+        image: "./MemberPhotos/No_Picture.png"
+    },
+    {
+        name: "Annika Acharya",
+        bio: "Annika is an 18-year-old business economics student with a passion for reading, writing, and volunteering. When she is not studying or lost in a good book, she loves listening to music, traveling, or playing sports.",
+        departments: "Social Media, Secretary",
+        country: "India",
+        image: "./MemberPhotos/Annika_Acharya.jpeg"
+    },
+    {
+        name: "Krishna Reddy",
+        bio: "Krishna is a passionate and dedicated individual, committed to innovation and meaningful impact. With creativity and determination, Krishna embodies the spirit of compassion and progress.",
+        departments: "Social Media",
+        country: "India",
+        image: "./MemberPhotos/Krishna_Reddy.jpg"
+    },
+    {
+        name: "Minahil Fahad Ilahi",
+        bio: "Driven by a vision of equitable healthcare and a deep commitment to improving global health, Minahil Fahad Ilahi aims to develop sustainable solutions to bridge the gap to the underserved communities. She is passionate about leveraging the power of biotechnology to address critical medical needs.",
+        departments: "Chapter Founder",
+        country: "Pakistan",
+        image: "./MemberPhotos/Minahil_Fahad_Ilahi.jpg"
+    },
+    {
+        name: "Daniel Soloshchenko",
+        bio: "A young and innovative entrepreneur and budding engineer, Daniel aims to create a future of collaboration between companies to ensure a brighter and better future, free of financial burdens, starting with Project MedMission.",
+        departments: "Finance",
+        country: "Canada",
+        image: "./MemberPhotos/Daniel_Soloshchenko.jpeg"
+    },
+    {
+        name: "Ayush Vasireddy",
+        bio: "Ayush Vasireddy is a sophomore, and is very passionate about science, hoping to pursue a career in neuroscience or biology. He is actively involved in scientific research, and in his free time enjoys playing tennis with his friends.",
+        departments: "Outreach and Recruiting, Secretary",
+        country: "USA",
+        image: "./MemberPhotos/Ayush_Vasireddy.png"
+    },
+    {
+        name: "Adil Mukhi",
+        bio: "Adil is a Grade 10 IB MYP student at Glenforest Secondary School who is dedicated to both academic success and effective leadership. He is a curious student with a passion for improvement, knowledge and trying new things. <br> \"With curiosity as my guide, I will never go wrong.\" https://adilmukhi.vercel.app/",
+        departments: "Finance",
+        country: "Canada",
+        image: "./MemberPhotos/Adil_Mukhi.jpeg"
+    },
+    {
+        name: "Oyinoluwa Adu",
+        bio: "A current student at Cardiff Sixth College, top 200 out of 20000, and on honours roll for the Fermat Competition by the University of Waterloo. Has researched 'feasibility of micro hydroelectric systems in Nigeria' and is an aspiring chemical engineer.",
+        departments: "Outreach and Recruiting",
+        country: "UK",
+        image: "./MemberPhotos/Oyinoluwa_Adu.jpg"
+    },
+    {
+        name: "Izzy Saraza",
+        bio: "Izzy is a high school student based in the Philippines. She has a keen interest in science communication and biomechanics. When not immersed in research or writing, she enjoys exploring creative pursuits and engaging in community initiatives.​",
+        departments: "Finance",
+        country: "Philippines",
+        image: "./MemberPhotos/Izzy_Saraza.png"
+    },
+    {
+        name: "Mandi Nguyen",
+        bio: "Mandi Nguyen is a Junior in her Early College High School majoring in Cybersecurity. Her passion for Volunteer work & marketing makes Project MedMission an amazing foundation she hopes to help flourish even more. She looks forward to many more successful projects and helping thousands.",
+        departments: "Social Media",
+        country: "USA",
+        image: "./MemberPhotos/Mandi_Nguyen.jpeg"
+    },
+    {
+        name: "Salim Surur",
+        bio: "Salim Surur is passionate about computer science and biology, applying both fields to develop machine learning projects with real-world impact through research. He specializes in web development and Python, particularly for machine learning, to solve problems at the intersection of technology and science. Salim is driven by a deep curiosity to tackle complex challenges through interdisciplinary approaches and focuses on meaningful work that bridges tech and research.",
+        departments: "Website Developer, Website Designer",
+        country: "USA",
+        image: "./MemberPhotos/No_Picture.png"
+    },
+    {
+        name: "Jason Yao",
+        bio: "Jason is a junior in high school interested in both biology and computer science. He is passionate about using his skills and knowledge to help others and make a positive impact.",
+        departments: "Website Developer",
+        country: "USA",
+        image: "./MemberPhotos/Jason_Yao.jpg"
+    },
+    {
+        name: "Paulette Newman",
+        bio: "Paulette a passionate and driven high school student with a strong interest in medicine, especially pediatric cardiology. She's involved in multiple youth-led organizations like The Ways of the Noggin, Project Medmission, and The Reconceive Project, where she combines creativity with science communication. Known for her dedication and leadership, she also volunteers as a tutor and mental health advocate. Paulette is committed to making a positive impact through both academic excellence and meaningful service.",
+        departments: "Social Media",
+        country: "USA",
+        image: "./MemberPhotos/Paulette_Newman.jpg"
+    },
+    {
+        name: "Sahil Patel",
+        bio: "Sahil, a high school student passionate about science, leads Project MedMission to drive innovative health solutions. He connects with communities to share knowledge, aiming to improve lives globally. With his expanding team, Sahil plans to repurpose over $100,000 in unused medications for impactful causes.",
+        departments: "Chapter Founder, Website Developer, Website Designer, Finance, Social Media, Outreach and Recruiting, Secretary, Editor",
+        country: "USA",
+        image: "./MemberPhotos/No_Picture.png"
+    }
+];
